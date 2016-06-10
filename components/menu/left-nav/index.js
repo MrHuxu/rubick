22eb7047f06cbed4f2d89685836fefa631869d5a
xhu <hxtheone@gmail.com>
2016-06-10 23:56:17 +0800

    fix some lint error

--- components/menu/left-nav/index.js
+++ components/menu/left-nav/index.js
@@ -5,7 +5,7 @@ import RaisedButton from 'material-ui/RaisedButton';
 
 export default class DrawerUndockedExample extends React.Component {
 
-  constructor(props) {
+  constructor (props) {
     super(props);
     this.state = {open: false};
   }
@@ -18,21 +18,21 @@ export default class DrawerUndockedExample extends React.Component {
     this.setState({open: false});
   }
 
-  render() {
+  render () {
     return (
       <div>
         <RaisedButton
-          label="Open Drawer"
-          onTouchTap={this.handleToggle}
+          label = 'Open Drawer'
+          onTouchTap = {this.handleToggle}
         />
         <Drawer
-          docked={false}
-          width={200}
-          open={this.state.open}
-          onRequestChange={(open) => this.setState({open})}
+          docked = {false}
+          width = {200}
+          open = {this.state.open}
+          onRequestChange = {(open) => this.setState({open})}
         >
-          <MenuItem onTouchTap={this.handleClose}>Menu Item</MenuItem>
-          <MenuItem onTouchTap={this.handleClose}>Menu Item 2</MenuItem>
+          <MenuItem onTouchTap = {this.handleClose}>Menu Item</MenuItem>
+          <MenuItem onTouchTap = {this.handleClose}>Menu Item 2</MenuItem>
         </Drawer>
       </div>
     );
